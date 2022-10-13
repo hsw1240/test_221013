068405bec3e268441be5a949fea74c97f54e88f0
SWHan83 <hsw1240@hermesys.co.kr>
2022-10-13 17:36:46 +0900

    2323

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
     console.log('첫 번째 미들웨어에서 요청을 처리함');
 
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.end('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.end('<h1>Express 서버에서 응답한 결과입니다.22222</h1>');
 });
 var port = process.env.PORT || 3000;
 http.createServer(app).listen(port, function(){
