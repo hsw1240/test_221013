9f8ec1b31707dba5701b40c881f7e1767eaaac9b
SWHan83 <hsw1240@hermesys.co.kr>
2022-10-13 18:16:13 +0900

    첫번째 commit/push

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(function(req, res, next) {
     console.log('첫 번째 미들웨어에서 요청을 처리함');
 
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.end('<h1>Express 서버에서 응답한 결과입니다.4444</h1>');
+    res.end('<h1>CI/CD 테스트 첫번째</h1>');
 });
 var port = process.env.PORT || 3000;
 http.createServer(app).listen(port, function(){
-    console.log('Express 서버가 3000번 포트에서 시작됨2');
+    console.log('Express 서버가 3000번 포트에서 시작됨');
 });
